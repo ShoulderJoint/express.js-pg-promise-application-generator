ae6c4a0f6c24dbcadcdfb66cca1c2005a22f857f
ShoulderJoint <misha.kondratev.2005@mail.ru>
2026-05-14 21:39:17 +0400

    trying to connect db with router

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
- res.json([
-    { id: 1, name: 'Petrov' },
-    { id: 2, name: 'Lelin' }
-  ]);
+router.get('/', async (req, res) => {
+  try {
+    const data = await db.any('SELECT * FROM staff');
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
