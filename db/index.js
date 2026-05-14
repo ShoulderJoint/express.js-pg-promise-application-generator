fa27fe5dc0a994204f5cee004120dc4941722b99
ShoulderJoint <misha.kondratev.2005@mail.ru>
2026-05-14 21:27:47 +0400

    initial commit

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const pgp = require('pg-promise')();
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'autoservice_kondratev',
+    user: 'postgres',
+    password: 90577567
+};
+const db = pgp(connection);
+
+module.exports = db;
